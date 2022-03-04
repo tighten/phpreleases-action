ef188d898a925223bba494c2aaf51d33ef450846
Kristin Collins <kristin@tighten.co>
2022-03-04 15:39:19 -0500

    Proof of concept for grabbing values from the API and populating the matrix.
    Co-author: Alison Kirk

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
-minimum=$(curl --url 'https://phpreleases.com/api/releases/minimum-supported/active')
-echo "::set-output name=releases::$minimum"
-latest=$(curl --url 'https://phpreleases.com/api/releases/latest')
-echo "::set-output name=releases::$latest%.*"
+Minimum=$(curl -k --url 'https://phpreleases.com/api/releases/minimum-supported/active' | jq -r '(.major|tostring) + "." + (.minor|tostring)')
+echo "::set-output name=minimum::Minimum"
+Latest=$(curl -k --url 'https://phpreleases.com/api/releases/latest' | jq -r 'split(".") | .[0] + "." + .[1]')
+echo "::set-output name=latest::${Latest}"
+Range=(${Minimum}, ${Latest})
+echo "::set-output name=range::${Range[*]}"
